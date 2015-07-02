e3750b02ac0ddd60d2e6d65ef9fe4e8037c19a94
Berton <sairly@126.com>
2015-07-02 17:16:11 +0800

    git ignore .idea

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onload = function(){
 
-    /* 
+    /*
       *轮播图高宽自适应，动态获取自适应后的高度，
-      *用这个高度渲染轮播图的容器，否则高度不匹配会影响下面的相邻元素 
+      *用这个高度渲染轮播图的容器，否则高度不匹配会影响下面的相邻元素
     */
     var carouselImage = document.querySelector('.carousel-item img');
     var carouselImageWrapper = document.querySelector('.carousel-area');
@@ -36,7 +36,7 @@ var setCookieItem = function(name,newValue,expire){
 console.log(getCookieItem('loginSuc'));
 
 /*
-  * 顶部“不再提示” 
+  * 顶部“不再提示”
 */
 var noMorePrompt_module = (function(){
     var prompt_bar = document.querySelector('.prompt-bar');
@@ -45,8 +45,8 @@ var noMorePrompt_module = (function(){
     if(reg_cookie_noMore.test(document.cookie)){
         prompt_bar.style.display = "none";
     }
-    no_more_btn.onclick = function(){  
-        addCookieItem('no_more_prompt','1',365);     
+    no_more_btn.onclick = function(){
+        addCookieItem('no_more_prompt','1',365);
         prompt_bar.style.marginTop = "-36px";
     };
 })();
@@ -75,7 +75,7 @@ var follow_login_module = (function(){
     follow_btn.onclick = function(){
         //判断是否已经登录
         if(reg_cookie_loginSuc.test(document.cookie)){
-           successFollow(); 
+           successFollow();
         }
         else {
             login_area.style.display = "block";
@@ -103,13 +103,13 @@ var carousel_module = (function(){
     var carousel_area = document.querySelector('.carousel-area');
     var carousel_indicator_area = document.querySelector('.carousel-indicators');
     var carousel_indicators = carousel_indicator_area.querySelectorAll('li');
-    var len = carousel_indicators.length; 
-    var initial_class_of_indicator = carousel_indicators[0].className; 
+    var len = carousel_indicators.length;
+    var initial_class_of_indicator = carousel_indicators[0].className;
 
     //初始化当前处于active状态的item与indicator,这里指定为第一个
     carousel_indicators[0].className = initial_class_of_indicator + " active";
     carousel_images[0].style.opacity = '1';
- 
+
     //获取当前处于active状态的索引
     var getCurrentIndex = function(){
         var currentIndex,i;
@@ -120,7 +120,7 @@ var carousel_module = (function(){
             }
         }
         return currentIndex;
-    }; 
+    };
 
     var carousel = function(targetIndex,currentIndex){
         //改变指示圆点的样式
@@ -143,9 +143,24 @@ var carousel_module = (function(){
         carousel(targetIndex,currentIndex);
     };
 
-    var beginAutoCarousel = setInterval(autoCarousel,5000); //每隔500ms自动切换
+    var beginAutoCarousel
+    var paused = true
+
+    var startCarousel = function() {
+        if (paused) {
+            beginAutoCarousel = setInterval(autoCarousel,1000);
+            paused = false
+        }
+    }
+
+    var pauseCarousel = function() {
+        clearInterval(beginAutoCarousel)
+        paused = true
+    }
+
 
     //用户手动切换，点击小圆点切换到对应的图片
+
     carousel_indicator_area.addEventListener('click',function(e){
         if(e.target.tagName == 'LI'){
             var targetIndex = parseInt(e.target.getAttribute('data-slide-to'));
@@ -155,13 +170,15 @@ var carousel_module = (function(){
     },false);
 
     carousel_area.onmouseover = function(){
-        clearInterval(beginAutoCarousel);
+        pauseCarousel()
     };
 
     carousel_area.onmouseout = function(){
-        setInterval(autoCarousel,5000);
+        startCarousel()
     };
-   
+
+    startCarousel()
+
 })();
 
 
